be7073cbd5e2ab045db4649caf299a0db4e8ffd0
BuildTools <unconfigured@null.spigotmc.org>
2017-04-08 15:49:55 -0400

    Added Challenge JOP

--- src/networking/Multiplayer.java
+++ src/networking/Multiplayer.java
@@ -1,9 +1,8 @@
 package networking;
 
-import java.awt.Container;
+import java.util.ArrayList;
 
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Multiplayer {
 	
@@ -12,12 +11,24 @@ public class Multiplayer {
 	}
 	
 	public Multiplayer(String username) {
-		JFrame f = new JFrame("Chess");
+		/*JFrame f = new JFrame("Chess");
 		
 		Container list = new Container();
 		list.setLayout(new BoxLayout(list, BoxLayout.PAGE_AXIS));
 		f.setSize(300,500);
 		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		JPanel UserList = new JPanel();
+		UserList.setLayout(new BoxLayout(UserList, BoxLayout.Y_AXIS));
+		JScrollPane pane = new JScrollPane(UserList);*/
+		
+		ArrayList players = new ArrayList();
+		for(int i=0;i<500;i++){
+			players.add("Opponent "+(i+1));
+		}
+		JOptionPane.showInputDialog(null, "Select Opponent", "Challenge", JOptionPane.QUESTION_MESSAGE, null, players.toArray(), null);
 	}
 	
 }
